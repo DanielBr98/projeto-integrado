e0a3d444745276c644e358d7412c67f2786871ae
Daniel <43521892+DanielBr98@users.noreply.github.com>
2018-11-25 15:48:25 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*var myIndex = 0;
 carousel();
 
-function carousel() {
+function carousel(){
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
@@ -12,3 +12,13 @@ function carousel() {
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 3000);
 }*/
+
+function Enviar() {
+ 
+    var nome = document.getElementById("name");
+ 
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+ 
+}
