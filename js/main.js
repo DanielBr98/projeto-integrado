72b0ed6b0125c7ed33bc310a041c3a3d994e5d03
Daniel <43521892+DanielBr98@users.noreply.github.com>
2018-11-25 17:10:03 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function Enviar() {
     var email = document.getElementById("email");
     var subject = document.getElementById("subject");
  
-    if (nome.value != "") {
+    if (nome.value != "" & email.value != "" & subject.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
  
