9d4460cd45808cdfa395d30249fce2c8799fd2a5
Daniel <43521892+DanielBr98@users.noreply.github.com>
2018-11-25 17:06:22 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@ function carousel(){
 function Enviar() {
  
     var nome = document.getElementById("name");
+    var email = document.getElementById("email");
+    var subject = document.getElementById("subject");
  
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
